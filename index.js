a97f561f69fd7a54be0dc81e1e6df5eb2f65e305
mehdibouchami <mehdibouchami77@gmail.com>
2020-12-30 16:44:09 +0100

    use cors

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routes/user');
 const mapsRouter = require('./routes/maps');
 const contrUser = require('./controllers/user');
+const { request } = require('express');
+var cors =  require('cors')
 
 const port = process.env.PORT || 3000;
 
@@ -12,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/uber', {useNewUrlParser: true,useUni
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
-
+app.use(cors());
 app.post('/api/login', contrUser.login);
 app.use('/api/user', userRouter);
 app.use('/api/maps', mapsRouter);
