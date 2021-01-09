adcf77303f4beef8ef2b5e409c1d7973bf75b167
mehdibouchami <mehdibouchami77@gmail.com>
2021-01-09 03:02:16 +0100

    socket

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const userRouter = require('./routes/user');
 const mapsRouter = require('./routes/maps');
 const contrUser = require('./controllers/user');
 const { request } = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
 var cors =  require('cors')
 
 const port = process.env.PORT || 3000;
@@ -18,6 +24,26 @@ app.use(cors());
 app.post('/api/login', contrUser.login);
 app.use('/api/user', userRouter);
 app.use('/api/maps', mapsRouter);
-app.listen(port, () => {
+
+let interval = [];
+
+io.on("connection", (socket) => {
+  console.log("New client connected "+socket.id);
+  if (interval[socket.id]) {
+    clearInterval(interval[socket.id]);
+  }
+  interval[socket.id] = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected "+socket.id);
+    clearInterval(interval[socket.id]);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+http.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
